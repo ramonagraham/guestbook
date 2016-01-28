6ddcb3a0d705629265daf67dcca6cf09754beb84
ramonagraham <rgraham8@mail.greenriver.edu>
2016-01-28 11:52:45 -0800

    final changes for lab04

--- guestbook/guestbook.js
+++ guestbook/guestbook.js
@@ -1,23 +1,61 @@
+Messages = new Mongo.Collection("message");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  
+  Meteor.subscribe("messages");  // every client created will subscribe to the same database
+  
+  Template.guestBook.helpers( {  // go to the Template called guestBook and call its helpers
+    "messages" : function () {
+      return  Messages.find(
+                  {},
+                  {sort: {createdOn: -1}} )  || {};  // this will query the database and find everything
+      // returns all message objects, or an empty object if no DB
     }
-  });
+    } );
+  
+  
+  Template.guestBook.events(  // Template is a variable, guestBook is an Object, events is a function
+      {   // events takes an object, this is an object
+        "submit form": function (event){  // listening for the submit event on a form is a css selector
+          event.preventDefault(); // overrides the GET
+          
+          // find the textarea, with jQuery
+          var messageBox =
+            $(event.target).find('textarea[name=guestBookMessage]');
+          
+          // console.log(messageBox);
+          var messageText = messageBox.val();
+          
+          var nameBox = $(event.target).find('input[name=guestName]');
+          var nameText = nameBox.val();
+          
+          Messages.insert(
+              {
+                name: nameText,
+                message: messageText,
+                createdOn: Date.now()
+              }
+          );
+          
+          nameBox.val("");
+          messageBox.val("");
+          
+       //   alert("Name is " + nameText + ", msg is " + messageText);
+        }
+      }
+  );
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  
+  Meteor.publish("messages", function () {
+    return Messages.find();
+    });
+  
+  
 }
